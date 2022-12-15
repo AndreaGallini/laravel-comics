9d0c157a41cd583de38b543b9b7222795685d075
AndreGallini <andrea.gallini01@libero.it>
2022-12-15 16:10:18 +0100

    done first page

--- resources/views/Partials/header.blade.php
+++ resources/views/Partials/header.blade.php
@@ -1,11 +1,68 @@
-<header>
+<header class="header">
     <div class="header_top"></div>
-    <div class="header_main">
+    <div class="header_main container-lg">
         <div class="header_left">
-            <img src="{{ Vite::asset('resources.img.dc-logo.png') }}" alt="">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
         </div>
-        <div class="header_center"></div>
-        <div class="header_right"></div>
+        <div class="header_center">
+            <nav class="navbar navbar-expand-lg navbar-light ">
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link my_link {{ Route::currentRouteName() == 'characters' ? 'active' : '' }}"
+                                href="#">characters </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link my_link {{ Route::currentRouteName() == 'home' ? 'active' : '' }}"
+                                href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link my_link" href="#">movies</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link my_link" href="#">tv</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link my_link" href="#">games</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link my_link" href="#">collectibles</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link my_link" href="#">videos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link my_link" href="#">fans</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link my_link" href="#">news</a>
+                        </li>
+                        <li class="nav-item dropdown my_link">
+                            <a class="nav-link dropdown-toggle my_link" href="#" id="navbarDropdownMenuLink"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Shop
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item my_link" href="#">Action</a>
+                                <a class="dropdown-item my_link" href="#">Another action</a>
+                                <a class="dropdown-item my_link" href="#">Something else here</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="header_right">
+            <form action="#">
+                <input type="text" name="" id="" placeholder="search">
+                <button type="submit">Cerca</button>
+            </form>
+        </div>
     </div>
 
 </header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,55 @@
 @extends('Layouts.app')
+
+@section('content')
+    <main class="jumbo_home_background">
+        <div class="jumbo">
+            <div class="current_series">
+                <p>Current series</p>
+            </div>
+        </div>
+        <div class="main ">
+            <div class="container-lg">
+                <section class="row">
+                    @foreach ($comics as $comic)
+                        <div class="col-2">
+                            <img src="{{ $comic['thumb'] }}" alt="">
+                            <p class="card_text">{{ $comic['series'] }}</p>
+                        </div>
+                    @endforeach
+                </section>
+                <section class="card_btn">
+                    <button class="my_btn">Load More</button>
+
+                </section>
+            </div>
+
+        </div>
+        <div class="bottom_main text-uppercase text-white">
+
+
+            <div class="container-lg bottom_main_inner">
+                <section class="my_card_bottom_main">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    <p>digital comics</p>
+                </section>
+                <section class="my_card_bottom_main">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    <p>dc merchandise</p>
+                </section>
+                <section class="my_card_bottom_main">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    <p>subscription</p>
+                </section>
+                <section class="my_card_bottom_main">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    <p>comic shop locator</p>
+                </section>
+                <section class="my_card_bottom_main">
+                    <img class="svg" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    <p>dc power visa</p>
+                </section>
+
+            </div>
+        </div>
+    </main>
+@endsection
